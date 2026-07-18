71e7239ecb2f4e1fc5faab54273ed852dbcadb08
松田淳司 <amatsuda@zaico.co.jp>
2026-07-18 14:18:57 +0900

    コメント追記

--- 2026/vue-jakunen2026/src/main.js
+++ 2026/vue-jakunen2026/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { router } from './router'
+import { router } from './router' // 追記
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app') // 変更
